f2269c2e7cf83d0852956785249bf029376bd0cc
Jeni Akbari <jeni.akbari@differenzsystem.com>
2025-07-07 15:06:27 +0530

    Implemented custom cors

--- src/index.js
+++ src/index.js
@@ -27,11 +27,24 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+// app.use(cors({
+//   origin: '*', 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');  
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 const httpServer = http.createServer(app);
 
